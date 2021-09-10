bdc1f207c4b08d03bc0dd4d991f47e4a906f47b4
Foley, David <david_foley1@dell.com>
2021-09-10 20:33:51 +0100

    Update elasticbean.tf

--- elasticbean/elasticbean.tf
+++ elasticbean/elasticbean.tf
@@ -1,7 +1,6 @@
 resource "aws_elastic_beanstalk_application" "devtest" {
   name        = "Flask-Dev"
   description = "Testing Flask App within PaaS"
-  solution_stack_name = "64bit Amazon Linux 2 v3.3.5 running Python 3.8"
 }
 
 resource "aws_elastic_beanstalk_environment" "testdev" {
